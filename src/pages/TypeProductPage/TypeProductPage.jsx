57983e7f0922be381eb8bc5d6fb848f855ce8c1f
unknown <duongduchuyaz@gmail.com>
2024-07-23 23:35:43 +0700

    fix Product page

--- src/pages/TypeProductPage/TypeProductPage.jsx
+++ src/pages/TypeProductPage/TypeProductPage.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
 import NavBarComponent from '../../components/NavBarComponent/NavBarComponent'
 import CardComponent from '../../components/CardComponent/CardComponent'
-import { Col, Row } from 'antd'
+import { Col, Pagination, Row } from 'antd'
 import { WrapperNavbar, WrapperProducts } from './style'
 
 const TypeProductPage = () => {
+  const onChange = () => {}
   return (
-    <Row style={{ padding: '0 120px', background: '#efefef', flexWrap: 'nowrap', paddingTop: '10px' }} >
-      <WrapperNavbar span={4}>
-        <NavBarComponent />
-      </WrapperNavbar>
-      <WrapperProducts span={20}>
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-      </WrapperProducts>
-    </Row>
+    <div style={{ padding: '0 120px', background: '#efefef' }}>
+      <Row style={{ flexWrap: 'nowrap', paddingTop: '10px' }} >
+        <WrapperNavbar span={4}>
+          <NavBarComponent />
+        </WrapperNavbar>
+        <Col span={20}>
+          <WrapperProducts>
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+            <CardComponent />
+          </WrapperProducts>
+          <Pagination defaultCurrent={2} total={100} onChange={onChange} style={{ textAlign: 'center', margin: '10px'}} />
+        </Col>
+      </Row>
+    </div>
   )
 }
 
