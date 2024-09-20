118fc1bc55b68fc52d919ec3b7e9a55eaf888766
unknown <duongduchuyaz@gmail.com>
2024-09-20 23:07:25 +0700

    navigate to type page in home page

--- src/routers/index.js
+++ src/routers/index.js
@@ -29,7 +29,7 @@ export const router = [
     isPrivate: false,
   },
   {
-    path: "/:type",
+    path: "/product/:type",
     page: TypeProductPage,
     isShowHeader: true,
     isPrivate: false,
